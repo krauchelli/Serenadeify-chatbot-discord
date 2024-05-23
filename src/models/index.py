a1bd8c6aa39d9dd2eb88a06be84e133884077bb5
Krauchelli <mekaku26shi@gmail.com>
2024-05-23 10:28:25 +0700

    initial commit, adding server as an api to processing model

---
+++ src/models/index.py
@@ -0,0 +1,20 @@
+from processing_model import preprocessing_predict
+from processing_model import predict_text
+import tensorflow as tf
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+#routes
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    print(data)
+    text = data['text']
+    prediction = predict_text(text)
+
+    return jsonify({'prediction': prediction})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
