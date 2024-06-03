039e073bbb4e7241491e41110e36f34f0a3cf0c8
Krauchelli <mekaku26shi@gmail.com>
2024-06-03 19:31:50 +0700

    Add endpoint for song recommendations

--- src/models/index.py
+++ src/models/index.py
@@ -1,7 +1,9 @@
-from processing_model import preprocessing_predict
-from processing_model import predict_text
 import tensorflow as tf
 from flask import Flask, request, jsonify
+# functions from other files
+from processing_model import preprocessing_predict
+from processing_model import predict_text
+from song_recommend import getRecommendation
 
 app = Flask(__name__)
 
@@ -15,6 +17,14 @@ def predict():
     print(f'printing prediction value: {prediction}')
     return jsonify({'prediction': prediction})
 
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    data = request.get_json()
+    print(data)
+    prediction = data['prediction']
+    recommendation = getRecommendation(prediction)
+    print(f'printing recommendation value: {recommendation}')
+    return jsonify({'recommendation': recommendation})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
